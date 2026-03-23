03d5ed41576baf9c4549ab2c94f20957bb544a8b
Walacy Pereira <wollygrin@gmail.com>
2026-03-23 14:17:57 -0300

    Implement mini sales system in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP Para <b>executar</b> o código, pressione <shortcut actionId="Run"/> ou
-// clique no ícone <icon src="AllIcons.Actions.Execute"/> no gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Pressione <shortcut actionId="ShowIntentionActions"/> com seu caret no texto destacado
-        // para ver como IntelliJ IDEA sugere corrigi-lo.
-        System.out.printf("Hello and welcome!");
+      Scanner sc = new Scanner(System.in);
+        System.out.println("### Praticando ###");
+        double totalVenda = 0.0;
+        int qtdItens = 0;
+        int op = -1;
+        while (op != 0) {
+            System.out.println("# Mini Sistema de Vendas #");
+            System.out.println("1-  Adicionar Pão (R$ 0.50)");
+            System.out.println("2 - Adicionar Leite (R$ 5.00)");
+            System.out.println("3 - Adicionar Café (R$ 15.00)");
+            System.out.println("4 - Ver Carrinho (Total de R$ e quantidade de itens)");
+            System.out.println("0 - Finalizar Compra.");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Pressione <shortcut actionId="Debug"/> para iniciar a depuração do seu código. Definimos um ponto de interrupção <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-            // para você, mas você sempre pode adicionar mais pressionando <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            op = sc.nextInt();
+            sc.nextLine();
+
+            switch (op){
+                case 1:
+                    System.out.println("Adicionando Pão ao carrinho");
+                    totalVenda += 0.50;
+                    qtdItens++;
+                    break;
+
+                case 2:
+                    System.out.println("Adicionando Leite, lá ele.");
+                    totalVenda += 5.00;
+                    qtdItens++;
+                    break;
+
+                case 3:
+                    System.out.println(" Adicionar Cafézin");
+                    totalVenda += 15.00;
+                    qtdItens++;
+                    break;
+
+                 case 4:
+                     System.out.println("Carrinho: " + qtdItens + " itens | Total: R$ " + totalVenda);
+                    break;
+
+                case 0:{
+                    System.out.println("Finalizando Compra...");
+                    break;
+                }
+
+                default:
+                    System.out.println("Opção invalida");
+            }
+
         }
+        if(totalVenda > 50.00){
+            double descontodepobre = totalVenda * 0.10;
+            totalVenda -= descontodepobre;
+            System.out.println("PARABÉNS! Você ganhou 10% de desconto!");
+            System.out.println("Valor do desconto: R$ " + descontodepobre);
+        }
+        System.out.println("\n=== COMPRA FINALIZADA ===");
+        System.out.println("Itens totais: " + qtdItens);
+        System.out.println("Valor a pagar: R$ " + totalVenda);
+        System.out.println("Obrigado pela preferência, volte sempre!");
+        sc.close();
     }
 }
